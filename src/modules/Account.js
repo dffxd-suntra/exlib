8891ef90345691b08de05b34b0b7adc7f48e7b90
Suntra <qawseswa1931@gmail.com>
2023-07-06 15:33:34 +0800

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	modified:   src/modules/Account.js

--- src/modules/Account.js
+++ src/modules/Account.js
@@ -1,7 +1,10 @@
 import config from "../config.js";
 
 export default class Account {
-    constructor() {
+    constructor(t) {
+        if (!t.constructor || (t.constructor != Object && t.constructor != Array)) {
+            throw new Error("");
+        }
     }
     add() {}
 }
